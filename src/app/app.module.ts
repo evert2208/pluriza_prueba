dd6d9eb43486fe33ef5444a03e04310defe52c70
evert2208 <65311634+evert2208@users.noreply.github.com>
2024-06-26 15:51:22 -0500

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './presentation/pages/dashboard/dashboard.component';
+import { BusquedaComponent } from './presentation/components/busqueda/busqueda.component';
+import { ListaUserComponent } from './presentation/components/lista-user/lista-user.component';
+import { NavbarComponent } from './presentation/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    BusquedaComponent,
+    ListaUserComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
